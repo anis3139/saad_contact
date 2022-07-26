ebfeb921dbb2131d76eacd4186b0fca8f72e3e2f
Md. Anichur Rahaman <38912435+anis3139@users.noreply.github.com>
2022-07-26 18:05:17 +0600

    single view change

--- includes/Admin/views/wp_contact-single.php
+++ includes/Admin/views/wp_contact-single.php
@@ -1,67 +1,89 @@
 <div class="wrap">
-    <h1><?php _e( 'Contacts', 'wp_contacts' ); ?></h1>
+    <h1><?php _e('Contacts', 'wp_contacts'); ?>
+    </h1>
 
-    <?php $item = wp_contacts_get_contact( $id ); ?>
+    <?php $item = wp_contacts_get_contact($id); ?>
 
-    <form action="" method="post">
 
-        <table class="form-table">
-            <tbody>
-                <tr class="row-name">
-                    <th scope="row">
-                        <label for="name"><?php _e( 'Name', 'wp_contacts' ); ?></label>
-                    </th>
-                    <td>
-                        <input type="text" name="name" id="name" class="regular-text" placeholder="<?php echo esc_attr( '', 'wp_contacts' ); ?>" value="<?php echo esc_attr( $item->name ); ?>" required="required" />
-                    </td>
-                </tr>
-                <tr class="row-email">
-                    <th scope="row">
-                        <label for="email"><?php _e( 'Email', 'wp_contacts' ); ?></label>
-                    </th>
-                    <td>
-                        <input type="text" name="email" id="email" class="regular-text" placeholder="<?php echo esc_attr( '', 'wp_contacts' ); ?>" value="<?php echo esc_attr( $item->email ); ?>" />
-                    </td>
-                </tr>
-                <tr class="row-phone">
-                    <th scope="row">
-                        <label for="phone"><?php _e( 'Phone Number', 'wp_contacts' ); ?></label>
-                    </th>
-                    <td>
-                        <input type="text" name="phone" id="phone" class="regular-text" placeholder="<?php echo esc_attr( '', 'wp_contacts' ); ?>" value="<?php echo esc_attr( $item->phone ); ?>" />
-                    </td>
-                </tr>
-                <tr class="row-subject">
-                    <th scope="row">
-                        <label for="subject"><?php _e( 'Subject', 'wp_contacts' ); ?></label>
-                    </th>
-                    <td>
-                        <input type="text" name="subject" id="subject" class="regular-text" placeholder="<?php echo esc_attr( '', 'wp_contacts' ); ?>" value="<?php echo esc_attr( $item->subject ); ?>" />
-                    </td>
-                </tr>
-                <tr class="row-message">
-                    <th scope="row">
-                        <label for="message"><?php _e( 'Message', 'wp_contacts' ); ?></label>
-                    </th>
-                    <td>
-                        <textarea name="message" id="message"placeholder="<?php echo esc_attr( '', 'wp_contacts' ); ?>" rows="5" cols="30"><?php echo esc_textarea( $item->message ); ?></textarea>
-                    </td>
-                </tr>
-                <tr class="row-created-by">
-                    <th scope="row">
-                        <label for="created_by"><?php _e( 'Created By', 'wp_contacts' ); ?></label>
-                    </th>
-                    <td>
-                        <input type="number" name="created_by" id="created_by" class="regular-text" placeholder="<?php echo esc_attr( '', 'wp_contacts' ); ?>" value="<?php echo esc_attr( $item->created_by ); ?>" />
-                    </td>
-                </tr>
-             </tbody>
-        </table>
+    <table class="form-table">
+        <tbody>
+            <tr class="row-name">
+                <th scope="row">
+                    <p><?php _e('Name', 'wp_contacts'); ?>
+                    </p>
+                </th>
+                <td>
+                    <p> <?php echo  $item->name ; ?>
+                    </p>
+                </td>
+            </tr>
+            <tr class="row-email">
+                <th scope="row">
+                    <p><?php _e('Email', 'wp_contacts'); ?>
+                    </p>
+                </th>
+                <td>
+                    <p> <?php echo  $item->email ; ?>
+                    </p>
+                </td>
+            </tr>
+            <tr class="row-phone">
+                <th scope="row">
+                    <p><?php _e('Phone Number', 'wp_contacts'); ?>
+                    </p>
+                </th>
+                <td>
+                    <p> <?php echo  $item->phone ; ?>
+                    </p>
+                </td>
+            </tr>
+            <tr class="row-subject">
+                <th scope="row">
+                    <p><?php _e('Subject', 'wp_contacts'); ?>
+                    </p>
+                </th>
+                <td>
+                    <p> <?php echo  $item->subject ; ?>
+                    </p>
+                </td>
+            </tr>
+            <tr class="row-message">
+                <th scope="row">
+                    <p><?php _e('Message', 'wp_contacts'); ?>
+                    </p>
+                </th>
+                <td>
+                    <p> <?php echo  $item->message ; ?>
+                    </p>
+                </td>
+            </tr>
+            <tr class="row-created-by">
+                <th scope="row">
+                    <p><?php _e('Created By', 'wp_contacts'); ?>
+                    </p>
+                </th>
+                <td>
+                    <p>
+                        <?php
+                            $user=get_user_by('id', $item->created_by);
+                            echo $user->user_firstname .' '. $user->user_lastname;
+                        ?>
+                    </p>
 
-        <input type="hidden" name="field_id" value="<?php echo $item->id; ?>">
+                </td>
+            </tr>
+            <tr class="row-created-at">
+                <th scope="row">
+                    <p><?php _e('Created At', 'wp_contacts'); ?>
+                    </p>
+                </th>
+                <td>
+                    <p>
+                        <?php echo date('M d, Y H:i a', strtotime($item->created_at));?>
+                    </p>
 
-        <?php wp_nonce_field( 'wp_contacts' ); ?>
-        <?php submit_button( __( 'Update Contact', 'wp_contacts' ), 'primary', 'wp_contact' ); ?>
-
-    </form>
+                </td>
+            </tr>
+        </tbody>
+    </table>
 </div>
