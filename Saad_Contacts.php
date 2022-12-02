66326a6a71fa1739c78e7b3c34218af096396bd6
Md. Anichur Rahaman <38912435+anis3139@users.noreply.github.com>
2022-12-02 11:55:13 +0600

    v2.0.1

--- Saad_Contacts.php
+++ Saad_Contacts.php
@@ -5,7 +5,7 @@
  * Plugin URI: https://github.com/anis3139
  * Author: Anichur Rahaman
  * Author URI: https://github.com/anis3139
- * Version: 2.0.0
+ * Version: 2.0.1
  * License: GPL2 or later
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  */
